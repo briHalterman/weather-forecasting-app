abca50867ffb8264e49f665bb732ff1fb0a19bb7
Brittany Halterman <halterma@gmail.com>
2025-01-26 16:06:31 -0500

    Adjust chart size, fail to debug missing negative values yet

--- app/services/chart_service.rb
+++ app/services/chart_service.rb
@@ -2,18 +2,13 @@ class ChartService
   BASE_URL = "https://image-charts.com/chart"
 
   def generate_temperature_chart(dates, highs, lows)
-    # Determine y-axis range dynamically
-    min_temp = [ highs.min, lows.min ].min - 5
-    max_temp = [ highs.max, lows.max ].max + 5
-
     query = {
       cht: "lc",
-      chs: "700x400",
       chd: "t:#{highs.join(',')}|#{lows.join(',')}",
       chxt: "x,y",
       chxl: "0:|#{dates.join('|')}",
-      chxr: "1,#{min_temp},#{max_temp}",
       chco: "FF0000,0000FF",
+      chs: "700x400",
       chtt: "Daily Highs and Lows"
     }
 
