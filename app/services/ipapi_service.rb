b4ceac1e2102cfaa07e241f34f26d157de8e866a
Brittany Halterman <halterma@gmail.com>
2025-01-26 14:54:34 -0500

    Improve ipapi service error handling

--- app/services/ipapi_service.rb
+++ app/services/ipapi_service.rb
@@ -5,14 +5,20 @@ class IpapiService
   base_uri "http://ip-api.com"
 
   def fetch_coordinates
-    query = { fields: "lat,lon" }
-    response = self.class.get("/json", query: query)
+    begin
+      query = { fields: "lat,lon" }
+      response = self.class.get("/json", query: query)
 
-    if response.success?
-      data = JSON.parse(response.body)
-      { latitude: data["lat"], longitude: data["lon"] }
-    else
-      { error: "Failed to fetch coordinates" }
+      if response.success?
+        data = JSON.parse(response.body)
+        if data["error"]
+          { error: "API error: #{data["error"]}" }
+        else
+        { latitude: data["lat"], longitude: data["lon"] }
+        end
+      else
+        { error: "Failed to fetch coordinates" }
+      end
     end
   end
 end
