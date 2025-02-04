1e2f591d42959d09377e007adb9b083b11cb79f5
Brittany Halterman <halterma@gmail.com>
2025-02-04 14:47:24 -0500

    Add system test for adding a location

---
+++ spec/system/locations_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe "Managing Locations", type: :system do
+  before do
+    driven_by(:rack_test) # Uses a headless browser
+  end
+
+  it "allows a user to add a location using coordinates" do
+    visit locations_path
+    click_link "Add Location from Coordinates"
+
+    fill_in "Location Name", with: "RoleModel Software"
+    fill_in "forecast_location[latitude]", with: "35.6879591"
+    fill_in "forecast_location[longitude]", with: "-78.8358726"
+
+    click_button "Add Location"
+
+    expect(page).to have_content("RoleModel Software")
+  end
+end
